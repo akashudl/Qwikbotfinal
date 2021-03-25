c24f78c23aa9ac56fddbd79cf442146c8a5602bb
akashudl <78961715+akashudl@users.noreply.github.com>
2021-03-25 22:10:50 +0530

    Update Userfinder.js

--- Client/src/Api/Userfinder.js
+++ Client/src/Api/Userfinder.js
@@ -1,6 +1,12 @@
   
 import axios from "axios";
 
+
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "/api/v1/user"
+    : "http://localhost:3006/api/v1/user";
+
 export default axios.create({
-  baseURL: "http://localhost:3006",
+  baseURL,
 });
